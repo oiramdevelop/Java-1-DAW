b6d4af9745825e73c732190ceb06b6d5fe598a64
Mario <oiramdeveloper@gmail.com>
2024-10-07 11:12:57 +0200

    Create ejemploComparadores.java

---
+++ Unidad1/ejemploComparadores.java
@@ -0,0 +1,18 @@
+package Unidad1;
+
+public class ejemploComparadores {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int edad=18;
+		String nombre  = "Emiliano";
+		float nota = 4.9f;
+		
+		
+		//El operador es cierto si el numero a la izquierda es mayor que el de la derecha 
+		//Estrictamente , si son iguales es falso
+		System.out.println("Es mayor que 18?"+ (edad>18));
+	}
+
+}
