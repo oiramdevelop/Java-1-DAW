877f741cf2f486e8f3bfd27aba86b307765cd714
Mario <oiramdeveloper@gmail.com>
2024-10-14 11:02:11 +0200

    Nuevo

--- Unidad1/EjemploOperadores.java
+++ Unidad1/EjemploOperadores.java
@@ -45,5 +45,6 @@ public class EjemploOperadores {
 		
 		System.out.println(mediaAlumnos);
 		
+		
 	}
 }
--- Unidad1/ejemploComparadores.java
+++ Unidad1/ejemploComparadores.java
@@ -8,12 +8,19 @@ public class ejemploComparadores {
 		int edad=18;
 		String nombre  = "Emiliano";
 		float nota = 4.9f;
-		int numModSup = 99;
+		int numModSup = 5;
 		boolean traeBebida = true;
 		int numAcompa = 2;
-		boolean zapatillas = true ;
+		boolean lleva_Zapatillas = true ;
 		//Al principio no puede entrar nadie
-		boolean puedeEntrarGad =false;
+		boolean puedeEntrarGrad =false;
+		boolean moduloAprob = false;
+		boolean becaTransp = false;
+		float rentaFam = 200000f;
+		int numeroFam = 4 ;
+		boolean empadronadoZonaFam = true;
+		boolean victimaTerro = false;
+		boolean victimaMaltra = false;
 		
 		
 		
@@ -35,9 +42,25 @@ public class ejemploComparadores {
 		
 		
 		//Combinamos todoas las condiciones con el operador logico y (&&)
+		puedeEntrarGrad = edad>18 && numModSup>=8 && traeBebida ==true && lleva_Zapatillas !=true;
 		
-		boolean puedeEntrarGrad = edad>18 && numModSup>=8 && traeBebida ==true && zapatillas !=true;
 		
+		
+		//Aprueba el modulo si es mayor de 17 años, y cumple de las siguientes condiciones
+		// La nota media es mayor de cuatro medio 4.5 o tiene mas de 6 modulos aprobados
+		moduloAprob = edad>17 && nota>4.5 || numModSup > 6;
+		
+		//El alumno consigue la beca de transporte si cumple que su renta familiar es inferior a 20mil  euros
+		// la familia tiene entre 3 y 6 personas , y no esta empadronado en otra provincia o cumple que es victima de 
+		// terrorismo o de maltrato
+		
+		//nota 1 : El operador ! (no) niega (resultado contrario) la condicion le sigue
+		// nota 2: Para comparar 
+		becaTransp = rentaFam <20000 && (numeroFam >= 3 && numeroFam <=6) && (empadronadoZonaFam == true  || victimaTerro == true || victimaMaltra == true );
+		
+		
+		System.out.println("Se le da la beca ?" +  becaTransp );
+		System.out.println("Ha aprobado el grado?" + moduloAprob);
 	}
 
 }
