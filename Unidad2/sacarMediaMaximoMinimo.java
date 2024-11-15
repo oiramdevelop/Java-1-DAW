fa19d302d214b70a21075ad93ba49e159b1c77c4
Mario <oiramdeveloper@gmail.com>
2024-11-15 13:13:02 +0100

    actu 15/11/2024

--- Unidad2/sacarMediaMaximoMinimo.java
+++ Unidad2/sacarMediaMaximoMinimo.java
@@ -9,28 +9,47 @@ public class sacarMediaMaximoMinimo {
 
 		Scanner teclado = new Scanner(System.in);
 
-		int i = 0;
-		int max = Integer.MIN_VALUE, min;
+		int i = 0, num = 0;
+		int max = Integer.MIN_VALUE, min = Integer.MAX_VALUE;
 		double media, suma = 0;
-
+		double factorial = 1;
 		while (i < 10) {
 			System.out.println("Introduce el numero " + (i + 1) + " : ");
-			int num = teclado.nextInt();
+			num = teclado.nextInt();
 
 			// Vamos sumando los numeros
 
 			suma = suma + num;
-
+			// Si el numero que leoo es mayor que el maximo hasta este momento , ese numero
+			// sera el nuevo maximo
 			if (num > max)
 				max = num;
+			// Para sacar el minimo valor vamos comprobando si el numero leido es menor que
+			// el min actual
+			if (num < min)
+				min = num;
 
 			i++;
+
+		}
+		// Leer un numero y mostrar su factorial y los divisores hasta 100
+		// El factoril de un numero es la multiplicacion de todos los numeros desde 1
+		// hasta dicho numero
+
+		System.out.println("Introduce un numero paraca calcular su factorial");
+		num = teclado.nextInt();
+		for (i = 1; i <= num; i++) {
+
+			factorial = i * factorial;
 
 		}
 		// Fuera del bucle dividimos la suma de los numeros entre
 		// La cantidad de elementos para calcular la media
 		media = suma / 10;
 		System.out.println("La media es : " + media);
+		System.out.println("El maximo es: " + max);
+		System.out.println("El minimo es: " + min);
+		System.out.println("El factorial de " + num + " es " + factorial);
 	}
 
 }
